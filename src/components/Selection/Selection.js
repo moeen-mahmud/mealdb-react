70d3ad7d7ba898147b6581dc82b9823a9547fa56
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-25 15:02:25 +0600

    added quantity functionality

--- src/components/Selection/Selection.js
+++ src/components/Selection/Selection.js
@@ -3,21 +3,17 @@ import "./Selection.css";
 
 const Selection = (props) => {
   const { selection } = props;
-
-  let mealQuantity = 0;
   let sameMeal = [];
   let distinctItem = [];
+  let mealCounts = {};
   let mealName = [];
   for (const meal of selection) {
-    if (!meal.quantity) {
-      meal.quantity = 1;
-    }
-    mealQuantity = mealQuantity + meal.quantity;
     mealName.push(meal.strMeal);
 
     if (sameMeal.indexOf(meal) === -1) {
       sameMeal.push(meal.strMeal);
     }
+
     for (const item of sameMeal) {
       if (distinctItem.indexOf(item) === -1) {
         distinctItem.push(item);
@@ -25,17 +21,28 @@ const Selection = (props) => {
     }
   }
 
+  sameMeal.forEach((x) => (mealCounts[x] = (mealCounts[x] || 0) + 1));
+  const onlyMeals = Object.values(mealCounts);
+
   console.log(sameMeal);
   console.log(distinctItem);
+  console.log(Object.values(mealCounts));
   return (
-    <div>
+    <div className="selection-area">
       <h3>Selected Meal: {distinctItem.length}</h3>
       <div className="selected-meal">
-        <ul>
+        <div>
+          <h3>Meal Name</h3>
           {distinctItem.map((name) => (
-            <li key={name.idMeal}>🍲 {name}</li>
+            <p key={name.idMeal}>🍲 {name}</p>
           ))}
-        </ul>
+        </div>
+        <div>
+          <h3>Quantity</h3>
+          {onlyMeals.map((count) => (
+            <p key={props.idMeal}>{count}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
