ffd3d75b6a795ce6089054c574e4c8ad1ff85d94
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-25 19:58:09 +0600

    updated Meals component

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,29 +1,43 @@
+//Importing Hooks
 import React, { useEffect, useState } from "react";
+
+//Importing CSS file
 import "./Meals.css";
+
+//Importing components
 import Meal from "../Meal/Meal";
 import Selection from "../Selection/Selection";
 
+//The meals component
 const Meals = () => {
+  //Get and set the states
   const [meals, setMeals] = useState([]);
   const [selection, setSelection] = useState([]);
   const [displayMeal, setDisplayMeal] = useState([]);
 
+  //Hooks for displaying initial foods
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
       .then((res) => res.json())
       .then((data) => setMeals(data.meals));
   }, []);
 
+  //Function for selecting a particular meal item
   const handleClick = (meal) => {
     const newSelection = [...selection, meal];
     setSelection(newSelection);
   };
 
+  //Function for search mechanics
   const handleChange = (event) => {
     const searchText = event.target.value;
     searchText.toLowerCase();
+
+    //Getting the api endpoints
     const endpointSearch = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     const endpointLetter = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`;
+
+    //Conditions for loading data from endpoints
     if (searchText.length === 1) {
       fetch(endpointLetter)
         .then((res) => res.json())
@@ -49,6 +63,7 @@ const Meals = () => {
 
   return (
     <>
+      {/* Search box */}
       <div className="search-area">
         <input
           type="text"
@@ -58,9 +73,11 @@ const Meals = () => {
           onChange={handleChange}
         />
       </div>
+
       <div className="all-meals">
         <div className="meal-area">
           <div>
+            {/* Displaying the search result from search and keep it */}
             {displayMeal.map((meal) => (
               <Meal
                 key={meal.idMeal}
@@ -70,6 +87,7 @@ const Meals = () => {
             ))}
           </div>
           <div>
+            {/* Displaying the initial meals after website load */}
             {meals.map((meal) => (
               <Meal
                 key={meal.idMeal}
@@ -80,6 +98,7 @@ const Meals = () => {
           </div>
         </div>
         <div className="selection-area">
+          {/* Selection component */}
           <Selection selection={selection}></Selection>
         </div>
       </div>
